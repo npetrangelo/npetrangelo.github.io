77e56ba89f2afe2c8389d2e6edb03d7fa80c13ac
Nathan Petrangelo <nathan@petrangelo.org>
2020-11-10 17:34:09 -0500

    Uploaded build

---
+++ build/precache-manifest.9228442da7fbffa4b005c8723054e727.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de5de8b5a0ec16e408894ea6765bb1b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "654df70895ffbe87702b",
+    "url": "/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "8bca13745ff66f664c0a",
+    "url": "/static/css/main.6b4c2253.chunk.css"
+  },
+  {
+    "revision": "654df70895ffbe87702b",
+    "url": "/static/js/2.b77f9ef6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b77f9ef6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bca13745ff66f664c0a",
+    "url": "/static/js/main.3b052631.chunk.js"
+  },
+  {
+    "revision": "89388c2853918f495cd0",
+    "url": "/static/js/runtime-main.574304e7.js"
+  },
+  {
+    "revision": "cfac64924f3bb9e434f5505fae647fc6",
+    "url": "/static/media/ABC.cfac6492.jpg"
+  },
+  {
+    "revision": "8424d7b45dae7157d5a3cc20d88307e7",
+    "url": "/static/media/Dino_Nugs.8424d7b4.jpg"
+  },
+  {
+    "revision": "cf45976ff91afd7a98e4789c0414c6a5",
+    "url": "/static/media/colorful-acrylic-paintings.cf45976f.jpg"
+  },
+  {
+    "revision": "41618d21f870c3702612dea70aacda22",
+    "url": "/static/media/download.41618d21.jfif"
+  },
+  {
+    "revision": "ffb91e1d8ce16fc57b32b9258a2e0bd6",
+    "url": "/static/media/images.ffb91e1d.jfif"
+  },
+  {
+    "revision": "f26332bfa0a0866a5ef67eb95edeb0c1",
+    "url": "/static/media/images2.f26332bf.jfif"
+  },
+  {
+    "revision": "05e75e89d4b0f51b4ef61aa8725f8b89",
+    "url": "/static/media/images3.05e75e89.jfif"
+  },
+  {
+    "revision": "2be2c7b095ab246d3b25bfbb775bba03",
+    "url": "/static/media/img1.2be2c7b0.jpg"
+  },
+  {
+    "revision": "5f1372939f286a74f295625b15752292",
+    "url": "/static/media/img2.5f137293.jpg"
+  },
+  {
+    "revision": "ed01003003ca328d098a08418f99bf03",
+    "url": "/static/media/img3.ed010030.jpg"
+  },
+  {
+    "revision": "df4149df8329e88a981f4fca6ff9497a",
+    "url": "/static/media/img4.df4149df.jpg"
+  },
+  {
+    "revision": "9b718666db489ff9dd663482334788b5",
+    "url": "/static/media/img5.9b718666.jpg"
+  },
+  {
+    "revision": "c8e6f38ade61baca1871cad97aca37a8",
+    "url": "/static/media/img6.c8e6f38a.jpg"
+  },
+  {
+    "revision": "8d8554ef869f22d5a7e2bc349a6ab638",
+    "url": "/static/media/kachow.8d8554ef.jpg"
+  },
+  {
+    "revision": "a2806e7dc26a79f13c8245ef404750f7",
+    "url": "/static/media/mountain-painting-chip-stoutus.a2806e7d.jpg"
+  },
+  {
+    "revision": "012f16269a44db802d90c49b3c46cddb",
+    "url": "/static/media/post1.012f1626.jpg"
+  },
+  {
+    "revision": "d1914527723fa88fb6a4b27c0c1f645e",
+    "url": "/static/media/post2.d1914527.jpg"
+  },
+  {
+    "revision": "67290891f4d3f3138effb2c59d81c26c",
+    "url": "/static/media/post6.67290891.jpg"
+  }
+]);
