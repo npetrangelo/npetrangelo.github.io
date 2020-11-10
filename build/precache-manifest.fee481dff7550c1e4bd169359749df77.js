f60e3a8d05b03d3d2939b51655367aac58e9e4eb
Nathan Petrangelo <nathan@petrangelo.org>
2020-11-10 17:40:30 -0500

    Updated build

---
+++ build/precache-manifest.fee481dff7550c1e4bd169359749df77.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b764627eb4c2a2ec5bb72b94b881781d",
+    "url": "/SWEN-444_Reduced_Tuition/index.html"
+  },
+  {
+    "revision": "654df70895ffbe87702b",
+    "url": "/SWEN-444_Reduced_Tuition/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "8bca13745ff66f664c0a",
+    "url": "/SWEN-444_Reduced_Tuition/static/css/main.6b4c2253.chunk.css"
+  },
+  {
+    "revision": "654df70895ffbe87702b",
+    "url": "/SWEN-444_Reduced_Tuition/static/js/2.b77f9ef6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/SWEN-444_Reduced_Tuition/static/js/2.b77f9ef6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bca13745ff66f664c0a",
+    "url": "/SWEN-444_Reduced_Tuition/static/js/main.3b052631.chunk.js"
+  },
+  {
+    "revision": "97d81b740a127ca2f40b",
+    "url": "/SWEN-444_Reduced_Tuition/static/js/runtime-main.66db5c77.js"
+  },
+  {
+    "revision": "cfac64924f3bb9e434f5505fae647fc6",
+    "url": "/SWEN-444_Reduced_Tuition/static/media/ABC.cfac6492.jpg"
+  },
+  {
+    "revision": "8424d7b45dae7157d5a3cc20d88307e7",
+    "url": "/SWEN-444_Reduced_Tuition/static/media/Dino_Nugs.8424d7b4.jpg"
+  },
+  {
+    "revision": "cf45976ff91afd7a98e4789c0414c6a5",
+    "url": "/SWEN-444_Reduced_Tuition/static/media/colorful-acrylic-paintings.cf45976f.jpg"
+  },
+  {
+    "revision": "41618d21f870c3702612dea70aacda22",
+    "url": "/SWEN-444_Reduced_Tuition/static/media/download.41618d21.jfif"
+  },
+  {
+    "revision": "ffb91e1d8ce16fc57b32b9258a2e0bd6",
+    "url": "/SWEN-444_Reduced_Tuition/static/media/images.ffb91e1d.jfif"
+  },
+  {
+    "revision": "f26332bfa0a0866a5ef67eb95edeb0c1",
+    "url": "/SWEN-444_Reduced_Tuition/static/media/images2.f26332bf.jfif"
+  },
+  {
+    "revision": "05e75e89d4b0f51b4ef61aa8725f8b89",
+    "url": "/SWEN-444_Reduced_Tuition/static/media/images3.05e75e89.jfif"
+  },
+  {
+    "revision": "2be2c7b095ab246d3b25bfbb775bba03",
+    "url": "/SWEN-444_Reduced_Tuition/static/media/img1.2be2c7b0.jpg"
+  },
+  {
+    "revision": "5f1372939f286a74f295625b15752292",
+    "url": "/SWEN-444_Reduced_Tuition/static/media/img2.5f137293.jpg"
+  },
+  {
+    "revision": "ed01003003ca328d098a08418f99bf03",
+    "url": "/SWEN-444_Reduced_Tuition/static/media/img3.ed010030.jpg"
+  },
+  {
+    "revision": "df4149df8329e88a981f4fca6ff9497a",
+    "url": "/SWEN-444_Reduced_Tuition/static/media/img4.df4149df.jpg"
+  },
+  {
+    "revision": "9b718666db489ff9dd663482334788b5",
+    "url": "/SWEN-444_Reduced_Tuition/static/media/img5.9b718666.jpg"
+  },
+  {
+    "revision": "c8e6f38ade61baca1871cad97aca37a8",
+    "url": "/SWEN-444_Reduced_Tuition/static/media/img6.c8e6f38a.jpg"
+  },
+  {
+    "revision": "8d8554ef869f22d5a7e2bc349a6ab638",
+    "url": "/SWEN-444_Reduced_Tuition/static/media/kachow.8d8554ef.jpg"
+  },
+  {
+    "revision": "a2806e7dc26a79f13c8245ef404750f7",
+    "url": "/SWEN-444_Reduced_Tuition/static/media/mountain-painting-chip-stoutus.a2806e7d.jpg"
+  },
+  {
+    "revision": "012f16269a44db802d90c49b3c46cddb",
+    "url": "/SWEN-444_Reduced_Tuition/static/media/post1.012f1626.jpg"
+  },
+  {
+    "revision": "d1914527723fa88fb6a4b27c0c1f645e",
+    "url": "/SWEN-444_Reduced_Tuition/static/media/post2.d1914527.jpg"
+  },
+  {
+    "revision": "67290891f4d3f3138effb2c59d81c26c",
+    "url": "/SWEN-444_Reduced_Tuition/static/media/post6.67290891.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9228442da7fbffa4b005c8723054e727.js"
+  "/SWEN-444_Reduced_Tuition/precache-manifest.fee481dff7550c1e4bd169359749df77.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SWEN-444_Reduced_Tuition/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
