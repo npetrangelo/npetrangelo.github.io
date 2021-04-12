51ccd98f063c6cd4b73ca3f66f21f1f54fc8e68c
Nathan Petrangelo <nathan@petrangelo.org>
2021-04-12 18:59:49 -0400

    Assignment 5 starter files

---
+++ assn5_transformation/cgIShape.js
@@ -0,0 +1,96 @@
+class cgIShape {
+    constructor () {
+        this.points = [];
+        this.bary = [];
+        this.indices = [];
+    }
+    
+    addTriangle (x0,y0,z0,x1,y1,z1,x2,y2,z2) {
+        var nverts = this.points.length / 4;
+        
+        // push first vertex
+        this.points.push(x0);  this.bary.push (1.0);
+        this.points.push(y0);  this.bary.push (0.0);
+        this.points.push(z0);  this.bary.push (0.0);
+        this.points.push(1.0);
+        this.indices.push(nverts);
+        nverts++;
+        
+        // push second vertex
+        this.points.push(x1); this.bary.push (0.0);
+        this.points.push(y1); this.bary.push (1.0);
+        this.points.push(z1); this.bary.push (0.0);
+        this.points.push(1.0);
+        this.indices.push(nverts);
+        nverts++
+        
+        // push third vertex
+        this.points.push(x2); this.bary.push (0.0);
+        this.points.push(y2); this.bary.push (0.0);
+        this.points.push(z2); this.bary.push (1.0);
+        this.points.push(1.0);
+        this.indices.push(nverts);
+        nverts++;
+    }
+}
+
+class Cube extends cgIShape {
+    
+    constructor (subdivisions) {
+        super();
+        this.makeCube (subdivisions);
+    }
+    
+    makeCube (subdivisions)  {
+        
+        // fill in your cube code here.
+    }
+}
+
+
+class Cylinder extends cgIShape {
+
+    constructor (radialdivision,heightdivision) {
+        super();
+        this.makeCylinder (radialdivision,heightdivision);
+    }
+    
+    makeCylinder (radialdivision,heightdivision){
+        // fill in your cylinder code here
+    }
+}
+
+class Cone extends cgIShape {
+
+    constructor (radialdivision, heightdivision) {
+        super();
+        this.makeCone (radialdivision, heightdivision);
+    }
+    
+    
+    makeCone (radialdivision, heightdivision) {
+    
+        // Fill in your cone code here.
+    }
+}
+    
+class Sphere extends cgIShape {
+
+    constructor (slices, stacks) {
+        super();
+        this.makeSphere (slices, stacks);
+    }
+    
+    makeSphere (slices, stacks) {
+        // fill in your sphere code here
+    }
+
+}
+
+
+function radians(degrees)
+{
+  var pi = Math.PI;
+  return degrees * (pi/180);
+}
+
