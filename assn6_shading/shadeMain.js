733e38a299b02f7d9a19006b4c0c9dbcd286fb1a
Nathan Petrangelo <nathan@petrangelo.org>
2021-05-03 01:46:32 -0400

    Moved camera closer

--- assn6_shading/shadeMain.js
+++ assn6_shading/shadeMain.js
@@ -142,7 +142,7 @@ function setUpCamera(program) {
     
     // set up your view
     let viewMatrix = glMatrix.mat4.create();
-    glMatrix.mat4.lookAt(viewMatrix, [0, 0, -5], [0, 0, 0], [0, 1, 0]);
+    glMatrix.mat4.lookAt(viewMatrix, [0, 0, -1], [0, 0, 0], [0, 1, 0]);
     gl.uniformMatrix4fv (program.uViewT, false, viewMatrix);
 }
 
