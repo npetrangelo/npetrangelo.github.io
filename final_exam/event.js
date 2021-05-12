62ae79665e1a8715f4101ddcb96f403aa8a98017
Nathan Petrangelo <nathan@petrangelo.org>
2021-05-12 13:37:36 -0400

    Reformatting

--- final_exam/event.js
+++ final_exam/event.js
@@ -1,12 +1,10 @@
-  'use strict';
+'use strict';
 
-  function gotKey (event) {
-      
-      var key = event.key;
-      
-      // Do something based on key press
-      
-      // create a new shape and do a redo a draw
-      draw();
-  }
-  
+function gotKey (event) {
+    var key = event.key;
+
+    // Do something based on key press
+
+    // create a new shape and do a redo a draw
+    draw();
+}
